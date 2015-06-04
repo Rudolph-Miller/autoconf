2ae589a13ff75d71a9796ddee7caa40fd66ed65a
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-05 03:49:40 +0900

    update version.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'chopsticks.tk.ppfm@gmail.com'
 license          'MIT'
 description      'Installs/Configures autoconf'
 long_description 'Installs/Configures autoconf'
-version          '0.1.1'
+version          '0.1.2'
 
 depends "m4"
